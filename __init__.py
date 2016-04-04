4b3fa0babb764400e9b12cf5ecb37a8583265691
Alexia Mondot <alexia.mondot@c-s.fr>
2016-04-04 16:47:12 +0200

    STYLE reformat code

--- __init__.py
+++ __init__.py
@@ -17,10 +17,12 @@
  ***************************************************************************/
  This script initializes the plugin, making it known to QGIS.
 """
+
+
 # load valuetool class from file valuetool.py
 
-def classFactory(iface): 
-  from valuetool import ValueTool
-  return ValueTool(iface)
+def classFactory(iface):
+    from valuetool import ValueTool
+    return ValueTool(iface)
 
 # Display the values of the raster layers at the current mouse position. Values are printed in a table or plotted on a graph. The plugin is dockable like overview or coordinate capture, go to View/Panels to activate it.
